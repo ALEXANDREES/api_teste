91ebe3f3330381e9672d0232d70e0681883a7a7b
Alexandre Henrique <alexandrehenrique.es@gmail.com>
2021-11-23 15:44:41 -0400

    🔧 CONFIG: Configuração da politica cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const routers = require('./controllers/index')
 const { configSequelize } = require('./models/index')
+const cors = require('cors')
 
 const app = express()
+
+const corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions))
 
 app.use(express.json())
 app.use('/', routers)
