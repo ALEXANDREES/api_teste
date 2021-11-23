0cdc6d1b3d3672f91caa4f05bdbade97c3af2221
Alexandre Henrique <alexandrehenrique.es@gmail.com>
2021-11-23 14:48:42 -0400

    🔨 REFACTOR: Correção da rota de cadastro para acesso publico

--- src/controllers/authentication.js
+++ src/controllers/authentication.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const { Users } = require('../models/index')
+const UserService = require('../services/users')
 const AuthenticationService = require('../services/authentication')
-const { check, validationResult } = require('express-validator')
+const { body, check, validationResult } = require('express-validator')
 const jwt = require('jsonwebtoken')
 const keyToken = require('../config/keyToken.json')
 const mailer = require('../util/mailer')
 
 const router = express.Router()
+const userService = new UserService(Users)
 const authenticationService = new AuthenticationService(Users)
 
 function generateToken(id = {}){
@@ -16,6 +18,46 @@ function generateToken(id = {}){
         {expiresIn: '12h'}
     )
 }
+
+router.post('/registration',
+    body('name').not().isEmpty().withMessage('The data sent cannot be empty or null').trim().escape(),   
+    check('cpf').not().isEmpty().isNumeric().withMessage('The data sent must be valid numbers'),
+    check('birthDate').not().isEmpty().isISO8601().toDate().withMessage('The date entered is not within the established pattern'),
+    check('password').not().isEmpty().isLength({ min: 6 }).withMessage('The password entered must have more than 6 characters'),
+    check('email').not().isEmpty().isEmail().withMessage('The e-mail entered is not a valid address'),
+
+    async (req, res) => {
+        const errors = validationResult(req)
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array()
+            })
+        }
+
+        const dataUser = {
+            name: req.body.name,
+            cpf: req.body.cpf,
+            birthDate: req.body.birthDate,
+            password: req.body.password,
+            email: req.body.email,
+            accessLevelId: req.body.accessLevelId
+        }
+
+        try {
+            await userService.add(dataUser)
+            res.status(201).send({
+                status: 201,
+                message: 'User successfully added!'
+            })
+        } catch (error) {
+            res.status(400).send({
+                status: 400,
+                message: error.message
+            })
+        }
+    }
+)
 
 router.post('/login',
     check('password').not().isEmpty().isLength({ min: 6 }).withMessage('The password entered must have more than 6 characters'),
